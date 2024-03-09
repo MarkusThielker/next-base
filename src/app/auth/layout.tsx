6b9246ad4eb2787dfbaea665bd08a4b54f904458
Markus Thielker <mail.markus.thielker@gmail.com>
2024-03-09 21:07:49 +0100

    N-BASE-1: add theme toggle to pages

--- src/app/auth/layout.tsx
+++ src/app/auth/layout.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { ThemeToggle } from '@/components/themeToggle';
 
 export default function AuthLayout({
     children,
@@ -6,7 +7,10 @@ export default function AuthLayout({
     children: React.ReactNode;
 }>) {
     return (
-        <div className="flex min-h-screen items-center justify-center">
+        <div className="flex min-h-screen items-center justify-center relative">
+            <div className="absolute top-4 right-4">
+                <ThemeToggle/>
+            </div>
             {children}
         </div>
     );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,15 +3,19 @@ import { User } from 'lucide-react';
 import React from 'react';
 import Link from 'next/link';
 import { URL_ACCOUNT } from '@/lib/constants';
+import { ThemeToggle } from '@/components/themeToggle';
 
 export default async function Home() {
     return (
-        <main className="flex min-h-screen flex-col items-center justify-center p-24 text-3xl space-y-4 relative">
-            <Button variant="ghost" size="icon" className="absolute top-4 right-4" asChild>
-                <Link href={URL_ACCOUNT}>
-                    <User/>
-                </Link>
-            </Button>
+        <main className="flex min-h-screen items-center justify-center text-3xl relative">
+            <div className="absolute flex items-center space-x-4 top-4 right-4">
+                <ThemeToggle/>
+                <Button variant="outline" size="icon" asChild>
+                    <Link href={URL_ACCOUNT}>
+                        <User/>
+                    </Link>
+                </Button>
+            </div>
             Next Base
         </main>
     );
